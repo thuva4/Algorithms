21ea64f6f4d82f278b3559da1ce3dd55fdd0686e
Harish Yerra <hyerra08@gmail.com>
2017-10-12 23:46:16 -0500

    Add GCD implementation in Python

--- SelectionSort/Python/selectionSort.py
+++ SelectionSort/Python/selectionSort.py
@@ -1,29 +1,15 @@
-def selection_sort(array):
-    """
-        Selection sort sorts an array by placing the minimum element element
-        at the beginning of an unsorted array.
-        :param array A given array
-        :return the given array sorted
-    """
+print("Enter the array:")
+x = map(int, raw_input().split())
 
-    length = len(array)
-
-    for i in range(0, length):
-        min_index = i # Suppose that the first (current) element is the minimum of the unsorted array
-
-        for j in range(i+1, length):
-            # Update min_index when a smaller minimum is found
-            if array[j] < array[min_index]:
-                min_index = j
-
-        if min_index != i:
-            # Swap the minimum and the initial minimum positions
-            array[min_index], array[i] = array[i], array[min_index]
-
-    return array
-
-# Example:
-if __name__ == '__main__':
-    example_array = [5, 6, 7, 8, 1, 2, 12, 14]
-    print(example_array)
-    print(selection_sort(example_array))
+for i in range(len(x)):
+    # Find the minimum element in remaining 
+    # unsorted array
+    min_idx = i
+    for j in range(i+1, len(x)):
+        if x[min_idx] > x[j]:
+            min_idx = j 
+    x[i], x[min_idx] = x[min_idx], x[i]
+ 
+print ("Sorted array")
+for i in range(len(x)):
+    print("%d" %x[i]), 
