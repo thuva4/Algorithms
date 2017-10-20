bf65d786f50ddda6a31252a28c0144ecf063ffc7
Swarnim Shukla <shuklaswarnim@gmail.com>
2017-10-21 01:34:28 +0530

    Added Bellmanford.py

--- BellmanFord/Python/BellmanFord.py
+++ BellmanFord/Python/BellmanFord.py
@@ -1,1 +1,50 @@
+from collections import defaultdict
+class Graph:
+ 
+    def __init__(self,vertices):
+        self.V= vertices #No. of vertices
+        self.graph = [] # default dictionary to store graph
+  
+    # function to add an edge to graph
+    def addEdge(self,u,v,w):
+        self.graph.append([u, v, w])
+         
+    # utility function used to print the solution
+    def printArr(self, dist):
+        print("Vertex   Distance from Source")
+        for i in range(self.V):
+            print("%d \t\t %d" % (i, dist[i]))
+     
+    def BellmanFord(self, src):
+ 
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+ 
+ 
+        for i in range(self.V - 1):
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+ 
+        for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        print "Graph contains negative weight cycle"
+                        return
+                         
+        # print all distance
+        self.printArr(dist)
+ 
+g = Graph(5)
+g.addEdge(0, 1, -1)
+g.addEdge(0, 2, 4)
+g.addEdge(1, 2, 3)
+g.addEdge(1, 3, 2)
+g.addEdge(1, 4, 2)
+g.addEdge(3, 2, 5)
+g.addEdge(3, 1, 1)
+g.addEdge(4, 3, -3)
+ 
+#Print the solution
+g.BellmanFord(0)
+ 
 #!/usr/bin/env python3
