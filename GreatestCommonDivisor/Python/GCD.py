4a5b023532490700b1412f8d10bcf6e407fad682
npcoder2k14 <npnarendrapal23@gmail.com>
2017-10-21 01:03:57 +0530

    Added GCD code in python which gives result in O(log n) time unlike the previous one whose time complexity was O(n)

--- GreatestCommonDivisor/Python/GCD.py
+++ GreatestCommonDivisor/Python/GCD.py
@@ -1,16 +1,17 @@
+"""
+This code is for GCD of two numbers in log(n) time.
+This code is compatible with python 2 as well as python3
+"""
+try:
+	input = raw_input
+except:
+	pass
 def gcd(a, b):
-    """
-       Calculates the GCD of the two parameters a and b. This method loops through all the numbers between 1 and
-       the smaller number to calculate the GCD.
-    """
+	if b == 0:
+		return a
+	return gcd(b , a%b)	    
 
-    smallerNumber = a if a < b else b
-    biggestFactor = 1
-    temp = 0
-
-    while temp < smallerNumber:
-        temp += 1
-        if a % temp == 0 and b % temp == 0:
-            biggestFactor = temp
-
-    return biggestFactor
+if __name__ == "__main__":
+	a,b = tuple(map(int , input("Enter two numbers for gcd: ").split(" ")))
+	print(a,b)
+	print("GCD of ",a," and ",b,"is",gcd(a,b))    
