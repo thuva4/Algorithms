c5a433a75cadfa5ffbaab46c2fc569d02b7638f7
Azkar Moulana <azkar.moulana@gmail.com>
2017-10-21 12:00:50 +0530

    created merge_sort

---
+++ MergeSort/Java/merge_sort.java
@@ -0,0 +1,60 @@
+public class Mergesort {
+    private int[] numbers;
+    private int[] helper;
+
+    private int number;
+
+    public void sort(int[] values) {
+        this.numbers = values;
+        number = values.length;
+        this.helper = new int[number];
+        mergesort(0, number - 1);
+    }
+
+    private void mergesort(int low, int high) {
+        // check if low is smaller than high, if not then the array is sorted
+        if (low < high) {
+            // Get the index of the element which is in the middle
+            int middle = low + (high - low) / 2;
+            // Sort the left side of the array
+            mergesort(low, middle);
+            // Sort the right side of the array
+            mergesort(middle + 1, high);
+            // Combine them both
+            merge(low, middle, high);
+        }
+    }
+
+    private void merge(int low, int middle, int high) {
+
+        // Copy both parts into the helper array
+        for (int i = low; i <= high; i++) {
+            helper[i] = numbers[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        // Copy the smallest values from either the left or the right side back
+        // to the original array
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
+                numbers[k] = helper[i];
+                i++;
+            } else {
+                numbers[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            numbers[k] = helper[i];
+            k++;
+            i++;
+        }
+        // Since we are sorting in-place any leftover elements from the right side
+        // are already at the right position.
+
+    }
+}
