01f02c51dcbcf68677556ffc4c2a678543e970f5
Kareliavs <karelia.vilca@ucsp.edu.pe>
2017-10-21 23:40:28 -0500

    Buuble sort c++ code

---
+++ BubbleSort/C++/BubbleSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> lista)
+{
+    for(int i=0; i < lista.size();i++)
+        cout<<lista[i]<<" ";
+}
+
+vector<int> BubbleSort( vector<int> data)
+{
+    int lon = data.size();
+
+    for (int i = 0; i < lon; ++i)
+    {
+        bool swapped = false;
+        for (int j = 0; j < lon - (i+1); ++j)
+        {
+            if (data[j] > data[j+1])
+            {
+                swap(data[j], data[j+1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+    return data;
+}
+
+
+int main()
+{
+    vector<int> data;
+    int num,tmp;
+
+    cout<<"number of elements: ";
+    cin>>num;
+    cout<<"elements: "<<endl;
+    for(int i=0; i<num;i++)
+    {
+        cin>>tmp;
+        data.push_back(tmp);
+    }
+
+    //Bubble sort
+    cout<<endl<<"Bubble sort"<<endl;
+    vector<int> bubble = BubbleSort(data);
+    print(bubble);
+    cout<<endl;
+    return 0;
+}
