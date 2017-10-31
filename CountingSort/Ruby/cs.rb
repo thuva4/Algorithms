81006c298618f52923abd4ca95004f72f533006f
Gargi Gupta <gargi15029@iiitd.ac.in>
2017-10-31 17:13:15 +0530

    Create cs.rb

---
+++ CountingSort/Ruby/cs.rb
@@ -0,0 +1,34 @@
+a = []
+def counting_sort(a,min=0,max=10)
+    if min>max
+      return "invalid range"
+    end
+    
+  	n=max-min+1
+	count=Array.new(n,0)
+	len=a.length
+	output=Array.new(len)
+  
+  	for i in 0...len
+		count[a[i]-min]+=1
+	end
+	
+  	for i in 1...n
+		count[i]+=count[i-1]
+	end
+		
+	
+	for i in 0...len
+		output[count[a[i]-min]-1]=a[i]
+		count[a[i]-min]-=1
+	end
+	
+	for i in 0...len
+		a[i]=output[i]
+	end
+	
+	return a
+	
+end
+
+puts(counting_sort(a,-3,10))
