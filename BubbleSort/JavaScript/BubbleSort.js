f551d90de5fabed9eccff483319a28e1486fd740
Jorge Leonardo Monge García <leomongeg@gmail.com>
2017-10-31 13:42:16 -0600

    Update BubbleSort.js
    Code improvement.

--- BubbleSort/JavaScript/BubbleSort.js
+++ BubbleSort/JavaScript/BubbleSort.js
@@ -1,5 +1,5 @@
 function bubbleSort(array) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0, length = array.length; i < length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] > array[j]) {
         swap(array, i, j);
