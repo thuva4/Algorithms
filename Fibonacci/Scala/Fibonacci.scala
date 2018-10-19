9587374060878463ecf00d284b4876df00153f23
thuva4 <tharma.thuva@gmail.com>
2018-10-19 10:28:42 +0530

    Change fib method

--- Fibonacci/Scala/Fibonacci.scala
+++ Fibonacci/Scala/Fibonacci.scala
@@ -1,18 +1,26 @@
-package recursion
+import scala.annotation.tailrec
 
 /**
  * Calculating a Fibonacci sequence recursively using Scala.
  */
  
-object Fibonacci extends App {
+object Fibonacci {
 
-    println(fib(1, 2))
-  
-    def fib(prevPrev: Int, prev: Int) {
-        val next = prevPrev + prev
-        println(next)
-        if (next > 1000000) System.exit(0)
-        fib(prev, next)
+    def fib(number: Int) : Int  = {
+        if (number == 0) {
+            0
+        }
+        else if(number == 1 ) {
+            1
+        } else {
+           fib(number-1) + fib(number-2)
+        }
     }
 
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 until 10){
+      println(fib(i))
+    }
+
+  }
 }
