0c7676291b9a887d344f6b4db5374b3023d376f3
Shuchita Jain <shuchitajain99@gmail.com>
2019-10-01 21:56:45 +0530

    C-Factorial: Improve formatting and remove extraneous long
    - Also improve output formatting

--- C/Factorial/Factorial.c
+++ C/Factorial/Factorial.c
@@ -1,30 +1,26 @@
-    #include<stdio.h>
-     
-    long factorial(int);
-     
-    int main()
-    {
-      int n;
-      long f;
-     
-      printf("Enter an integer to find its factorial\n");
-      scanf("%d", &n);
-     
-      if (n < 0)
-        printf("Factorial of negative integers isn't defined.\n");
-      else
-      {
-        f = factorial(n);
-        printf("%d! = %ld\n", n, f);
-      }
-     
-      return 0;
-    }
-     
-    long factorial(int n)
-    {
-      if (n == 0)
-        return 1;
-      else
-        return(n * factorial(n-1));
-    }
+#include <stdio.h>
+
+long factorial(int);
+
+int main()
+{
+  int n;
+
+  printf("Enter an integer to find its factorial: ");
+  scanf("%d", &n);
+
+  if (n < 0)
+    printf("Factorial of negative integers isn't defined.\n");
+  else
+    printf("%d! = %ld\n", n, factorial(n));
+
+  return 0;
+}
+
+long factorial(int n)
+{
+  if (n == 0)
+    return 1;
+  else
+    return(n * factorial(n-1));
+}
