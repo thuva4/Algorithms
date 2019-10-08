8232193342fbfabbb6b3a11c36489755588e6337
Anurag Natoo <35635290+anuragnatoo@users.noreply.github.com>
2019-10-08 10:56:19 +0530

    Resolved a small error in Linear Search C++
    Resolved a small error(return 0 without a main function) and added a example

--- C++/LinearSearch/LinearSearch.cpp
+++ C++/LinearSearch/LinearSearch.cpp
@@ -1,9 +1,6 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-
-// Linearly search x in arr[].  If x is present then return its 
+// Linearly search x in arr[].  If x is present then return its
 // location,  otherwise return -1
 int search(int arr[], int n, int x)
 {
@@ -14,5 +11,21 @@ int search(int arr[], int n, int x)
     return -1;
 }
 
-return 0;
+int main()
+{
+    int a[10]={97,45,32,54,66,29,19,8,87,22};
+    int x;
+    int position;
+    cout<<"Enter the element to search for"<<endl;
+    cin>>x;
+    position=search(a,10,x);
+    if(position==-1)
+    {
+        cout<<x<<" is not present in the array "<<endl;
+    }
+    else
+    {
+        cout<<x<<" is present at position "<<position<<endl;
+    }
+    return 0;
 }
