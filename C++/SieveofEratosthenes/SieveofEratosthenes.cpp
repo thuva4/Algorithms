1029dab84143ed70fcd16e2d24db9031f98409fd
amit <amittiwary710@gmail.com>
2019-10-12 20:54:44 +0530

    included comments

--- C++/SieveofEratosthenes/SieveofEratosthenes.cpp
+++ C++/SieveofEratosthenes/SieveofEratosthenes.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 //This code will compute all the prime numbers
 // that are smaller than or equal to N.
-
+//beginning of Sieve function
 void sieve(int N) {
         bool isPrime[N+1];
+
+	//Initialising all the array elements with 'true'
+	
         for(int i = 0; i <= N;++i) {
             isPrime[i] = true;
         }
+
+	//since 0 and 1 are non-prime numbers.
         isPrime[0] = false;
         isPrime[1] = false;
+
+	
         for(int i = 2; i * i <= N; ++i) {
              if(isPrime[i] == true) {                    //Mark all the multiples of i as composite numbers
                  for(int j = i * i; j <= N ;j += i)
@@ -19,4 +26,4 @@ void sieve(int N) {
             }
         }
     }
-
+//closing of function
