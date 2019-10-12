11462555ce4e6fcc127d807eb21687c04d4288df
Shuchita Jain <shuchitajain99@gmail.com>
2019-10-13 02:33:43 +0530

    Added reversing algorithm

---
+++ Python/Reverse.py
@@ -0,0 +1,12 @@
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+
+s = "Python"
+
+print ("The original string  is : ",end="")
+print (s)
+print ("The reversed string(using loops) is : ",end="")
+print (reverse(s))
