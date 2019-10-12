26692c594c1e3efcdf2c332c344802b5bab5ac9a
Shuchita Jain <shuchitajain99@gmail.com>
2019-10-13 02:58:58 +0530

    Added another algo for Fibonacci

--- Python/Fibonacci/Fibonacci.py
+++ Python/Fibonacci/Fibonacci.py
@@ -17,3 +17,16 @@ def fibonacci(num):
         nb1, nb2 = nb2, nb1 + nb2
 
     return nb2
+
+# Using Dynamic Programming
+FibArray = [0,1]
+def fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    elif n<=len(FibArray):
+        return FibArray[n-1]
+    else:
+        temp_fib = fibonacci(n-1)+fibonacci(n-2)
+        FibArray.append(temp_fib)
+        return temp_fib
+print(fibonacci(9))
