0281189bd8834a1d06df2c852b04c31ca02a4f13
serturx <serturx@users.noreply.github.com>
2019-10-16 03:32:10 +0200

    Added Cocktail Sort in Java

---
+++ Java/CocktailSort/CocktailSort.java
@@ -0,0 +1,35 @@
+public class CocktailSort {
+  static int[] cSort(int[] toSort){
+    int buffer = 0;
+    int start = -1;
+    int end = toSort.length - 2;
+    boolean swapped; //Finished sorting when nothing has been swapped
+    do {
+      swapped = false;
+      start++; //Moving lower border
+      
+      for(int i = start; i <= end; i++) { //Sorting upwards
+        if(toSort[i] > toSort[i + 1]) {
+          buffer = toSort[i];
+          toSort[i] = toSort[i + 1];
+          toSort[i + 1] = buffer;
+          swapped = true;
+        }
+      }
+
+      if(swapped == false) break;
+      swapped = false;
+      end--; //Moving upper border
+
+      for(int i = end; i >= start; i--) { //Sorting downwards
+        if(toSort[i] > toSort[i + 1]) {
+          buffer = toSort[i];
+          toSort[i] = toSort[i + 1];
+          toSort[i + 1] = buffer;
+          swapped = true;
+        }
+      }
+    } while(swapped);
+    return toSort;
+  }
+}
