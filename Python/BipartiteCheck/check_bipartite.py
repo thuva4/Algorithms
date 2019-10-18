1baaccc6ca9247a896437ebb7da4a1aa93a51277
Sumit Jain <42279031+sumitjain0695@users.noreply.github.com>
2019-10-18 23:44:37 +0530

    Create check_bipartite.py

---
+++ Python/BipartiteCheck/check_bipartite.py
@@ -0,0 +1,28 @@
+def check_bipartite(adj_list):
+
+    V = len(adj_list)
+
+    # Divide vertexes in the graph into set_type 1 and 2
+    # Initialize all set_types as -1
+    set_type = [-1 for v in range(V)]
+    set_type[0] = 0
+
+    q = [0]
+
+    while q:
+        v = q.pop(0)
+
+        # If there is a self-loop, it cannot be bipartite
+        if adj_list[v][v]:
+            return False
+
+        for u in range(V):
+            if adj_list[v][u]:
+                if set_type[u] == set_type[v]:
+                    return False
+                elif set_type[u] == -1:
+                    # set type of u opposite of v
+                    set_type[u] = 1 - set_type[v]
+                    q.append(u)
+
+    return True
