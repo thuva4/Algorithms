6888e3036fcbab7976c7ed17e70c82e31752da2c
Kshitij-Saluja <43849791+Kshitij-Saluja@users.noreply.github.com>
2019-10-22 21:37:08 +0530

    Update fibonacci.c

--- C/Fibonacci/fibonacci.c
+++ C/Fibonacci/fibonacci.c
@@ -13,8 +13,16 @@ int main() {
     printf("%d, ", num1);
     // This calculates fibonacci number;
     temp = num1 + num2;
+    //if the sum becomes greater than range of int.
+    if(num1>temp || num2 >temp){
+        break;
+    }
     num1 = num2;
     num2 = temp;
+  }
+    //the case for which break is encounterd
+  if(i != v){
+    cout<<"size became bigger than size of int";
   }
   return 0;
 }
