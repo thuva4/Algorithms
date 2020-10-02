4087a7fcf31c277ed91540cc56ccb2cfb3375613
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-10-02 11:56:59 +0200

    Added Assert

--- algorithms/C/PrimeCheck/primeCheck.c
+++ algorithms/C/PrimeCheck/primeCheck.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 bool isPrimeNumber(int n)
 {
     if (n <= 3)
     {
-        return(n > 1);
+        return (n > 1);
     }
     else if (n % 2 == 0 || n % 3 == 0)
     {
-        return(false);
+        return (false);
     }
 
     int i = 5;
@@ -18,21 +19,21 @@ bool isPrimeNumber(int n)
     {
         if (n % i == 0 || n % (i + 2) == 0)
         {
-            return(false);
+            return (false);
         }
         i += 6;
     }
 
-    return(true);
+    return (true);
 }
 
 int main()
 {
-    printf("%i\n", isPrimeNumber(11));
-    printf("%i\n", isPrimeNumber(12));
-    printf("%i\n", isPrimeNumber(13));
-    printf("%i\n", isPrimeNumber(15));
-    printf("%i\n", isPrimeNumber(17));
-    printf("%i\n", isPrimeNumber(19));
-    return(0);
+    assert(isPrimeNumber(11));
+    assert(!isPrimeNumber(12));
+    assert(isPrimeNumber(13));
+    assert(!isPrimeNumber(15));
+    assert(isPrimeNumber(17));
+    assert(isPrimeNumber(19));
+    return (0);
 }
