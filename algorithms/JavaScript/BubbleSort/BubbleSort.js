6901ccafc5ef1d5205166ae37653e48a794d27ac
Thuvarakan Tharmarajasingam <thuva4.dev@gmail.com>
2020-10-03 15:15:02 +0530

    add tests for js algos

--- algorithms/JavaScript/BubbleSort/BubbleSort.js
+++ algorithms/JavaScript/BubbleSort/BubbleSort.js
@@ -1,3 +1,10 @@
+/**
+ * Sort array in O(n^2)
+ * Bubble Sort will be faster for small number of elements 
+ * In-place sort without extra space
+ * @param  {Array}  arr Array to search into
+ * @return {Array} Sorted array
+ */
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
@@ -6,12 +13,14 @@ function bubbleSort(array) {
       }
     }
   }
-
   return array;
 }
 
 function swap(array, firstIndex, secondIndex) {
-  const tmp = array[firstIndex];
-  array[firstIndex] = array[secondIndex];
-  array[secondIndex] = tmp;
+  array[firstIndex] = array[firstIndex] ^ array[secondIndex];
+  array[secondIndex] = array[firstIndex] ^ array[secondIndex];
+  array[firstIndex] = array[firstIndex] ^ array[secondIndex];
 }
+
+
+module.exports = bubbleSort;
