542a2244038e3e2993800d0db24494bcb8701e3e
Maria Luísa Mendes <72216924+lumendesp@users.noreply.github.com>
2020-10-03 16:52:52 -0300

    Update bubblesort.c
    I implemented the option to choose the length and elements of the array and select the type of sorting.

--- algorithms/C/BubbleSort/bubblesort.c
+++ algorithms/C/BubbleSort/bubblesort.c
@@ -1,38 +1,71 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-int n,i,j,temp;
-printf("Enter number of elements in array\n");
-scanf("%d",&n);
-int A[n];
-printf("Array before sorting\n");
+void bubbleSort(int array[], int size, int order){
+  int aux = 0;
 
-for(i=0;i<n;i++)
-	{
-	A[i]=rand()%1000;
-	printf("%d ",A[i]);	
-	}
-printf("\n");
+  if(order == 1){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
 
-for(i=0;i<n;i++)
-	{
-	for(j=n;j>i;j--)
-		{
-		  if(A[j]<A[j-1])
-			{
-			temp=A[j];
-			A[i]=A[i-1];
-			A[i-1]=temp;
-			
-			}	
-		}	
-	}
-printf("Array after sorting\n");
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] > array[j+1]){
+          aux = array[j]; 
+          array[j] = array[j+1]; 
+          array[j+1] = aux; 
+          flag = 1;
+        }
+      }
 
-for(i=0;i<n;i++) printf("%d ",A[i]);
+      if(flag == 0){
+        break;
+      }
+    }
+  }
 
-printf("\n");
-	
-return 0;
+  else if(order == 2){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] < array[j+1]){
+          aux = array[j+1];
+          array[j+1] = array[j];
+          array[j] = aux;
+          flag = 1;
+        }
+      }
+
+      if(flag == 0){
+        break;
+      }
+    }
+  }
+
+}
+
+int main() {
+  int size;
+  int order;
+
+  printf("Enter the size of the array:\n");
+	scanf("%i", &size);
+
+	int array[size];
+
+	printf("Enter the elements of the array:\n");
+	for(int i = 0; i < size; i++){
+	  scanf("%i", &array[i]);
+  }
+
+  printf("What type of ordering do you want: \n 1 - Ascending \n 2 - Descending\n");
+  scanf("%i", &order);
+
+	bubbleSort(array, size, order);
+
+	printf("The sorted array is:\n");
+	for (int i = 0; i < size; i++){
+    printf("%i ", array[i]);
+  }
+
+  return 0;
 }
