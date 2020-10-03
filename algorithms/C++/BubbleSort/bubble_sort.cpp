18136c79b1ed152c6e25325cccfb3c2e6db61563
Maria Luísa Mendes <72216924+lumendesp@users.noreply.github.com>
2020-10-03 16:34:38 -0300

    Update bubble_sort.cpp
    I implemented the option to choose the length and elements of the array and select the type of sorting.

--- algorithms/C++/BubbleSort/bubble_sort.cpp
+++ algorithms/C++/BubbleSort/bubble_sort.cpp
@@ -1,42 +1,67 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
- 
+void bubbleSort(int array[], int size, int order){
 
-void bubbleSort(int arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)       
-       for (j = 0; j < n-i-1; j++) 
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
-}
- 
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        cout<<arr[i]<<" ";
-}
- 
-int main()
-{
-    int arr[100],n;
-    cout<<"Enter the size of array : \n";
-    cin>>n;
-    cout<<"Enter the elements : \n";
-    for(int i=0 ; i<n ; i++){
-        cin>>arr[i];
+  if(order == 1){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] > array[j+1]){
+          swap(array[j+1], array[j]);
+          flag = 1;
         }
-    //int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    cout<<"Sorted Array : \n";
-    printArray(arr, n);
-    return 0;
+      }
+
+      if(flag == 0){
+        break;
+      }
+    }
+  }
+
+  else if(order == 2){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] < array[j+1]){
+          swap(array[j+1], array[j]);
+          flag = 1;
+        }
+      }
+
+      if(flag == 0){
+        break;
+      }
+    }
+  }
+
+}
+
+int main() {
+  int size;
+  int order;
+
+  cout << "Enter the size of the array:" << endl;
+	cin >> size;
+
+	int array[size];
+
+	cout << "Enter the elements of the array:" << endl;
+	for(int i = 0; i < size; i++){
+	  cin >> array[i];
+  }
+
+  cout << "What type of ordering do you want: \n 1 - Ascending \n 2 - Descending" << endl;
+  cin >> order;
+
+	bubbleSort(array, size, order);
+
+	cout << "The sorted array is:" <<endl;
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << " ";
+  }
+
+  return 0;
 }
