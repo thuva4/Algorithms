2925791afa856173dcf4ad69dfc6b04c6677b800
Omkarnath Parida <pomkarnath98@gmail.com>
2020-10-04 23:13:34 +0530

    Style Changed

--- algorithms/JavaScript/ZeroOneKnapsack/ZeroOneKnapsack.js
+++ algorithms/JavaScript/ZeroOneKnapsack/ZeroOneKnapsack.js
@@ -5,18 +5,18 @@
 
 function zeroOneKnapsack(arr, n, cap, cache) {
   if (cap === 0 || n === 0) {
-    cache[n][cap] = 0
-    return cache[n][cap]
+    cache[n][cap] = 0;
+    return cache[n][cap];
   }
   if (cache[n][cap] !== -1) {
-    return cache[n][cap]
+    return cache[n][cap];
   }
   if (arr[n - 1][0] <= cap) {
-    cache[n][cap] = Math.max(arr[n - 1][1] + zeroOneKnapsack(arr, n - 1, cap - arr[n - 1][0], cache), zeroOneKnapsack(arr, n - 1, cap, cache))
-    return cache[n][cap]
+    cache[n][cap] = Math.max(arr[n - 1][1] + zeroOneKnapsack(arr, n - 1, cap - arr[n - 1][0], cache), zeroOneKnapsack(arr, n - 1, cap, cache));
+    return cache[n][cap];
   } else {
-    cache[n][cap] = zeroOneKnapsack(arr, n - 1, cap, cache)
-    return cache[n][cap]
+    cache[n][cap] = zeroOneKnapsack(arr, n - 1, cap, cache);
+    return cache[n][cap];
   }
 }
 
@@ -34,37 +34,37 @@ function main() {
       2 4
       3 0
       2 5
-      2 3`
+      2 3`;
 
-  input = input.trim().split('\n')
-  input.shift()
-  const length = input.length
+  input = input.trim().split('\n');
+  input.shift();
+  const length = input.length;
 
-  let i = 0
+  let i = 0;
   while (i < length) {
-    const cap = Number(input[i].trim().split(' ')[0])
-    const currlen = Number(input[i].trim().split(' ')[1])
-    let j = i + 1
-    const arr = []
+    const cap = Number(input[i].trim().split(' ')[0]);
+    const currlen = Number(input[i].trim().split(' ')[1]);
+    let j = i + 1;
+    const arr = [];
     while (j <= i + currlen) {
-      arr.push(input[j])
-      j++
+      arr.push(input[j]);
+      j++;
     }
-    const newArr = []
+    const newArr = [];
     arr.forEach(e => {
-      newArr.push(e.trim().split(' ').map(Number))
+      newArr.push(e.trim().split(' ').map(Number));
     })
-    const cache = []
+    const cache = [];
     for (let i = 0; i <= currlen; i++) {
-      const temp = []
+      const temp = [];
       for (let j = 0; j <= cap; j++) {
-        temp.push(-1)
+        temp.push(-1);
       }
-      cache.push(temp)
+      cache.push(temp);
     }
-    const result = zeroOneKnapsack(newArr, currlen, cap, cache)
-    console.log(result)
-    i += currlen + 1
+    const result = zeroOneKnapsack(newArr, currlen, cap, cache);
+    console.log(result);
+    i += currlen + 1;
   }
 }
 
