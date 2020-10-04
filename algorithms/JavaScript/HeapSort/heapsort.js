fa77f318a729d5bc4b0fdd7a8154db6a2081fca4
Thuvarakan Tharmarajasingam <17289840+Thuva4@users.noreply.github.com>
2020-10-04 15:08:57 +0530

    Js code samples (#804)
    * add tests for js algos
    
    * add more tests
    
    * add tests
    
    Co-authored-by: Thuvarakan Tharmarajasingam <thuva4.dev@gmail.com>

--- algorithms/JavaScript/HeapSort/heapsort.js
+++ algorithms/JavaScript/HeapSort/heapsort.js
@@ -1,30 +1,31 @@
-/*
-* Build a max heap out of the array. A heap is a specialized tree like
-* data structure that satisfies the heap property. The heap property
-* for max heap is the following: "if P is a parent node of C, then the
-* key (the value) of node P is greater than the key of node C"
-* Source: https://en.wikipedia.org/wiki/Heap_(data_structure)
-*/
-Array.prototype.heapify = function (index, heapSize) {
+/** 
+ * Build a max heap out of the array. A heap is a specialized tree like
+ * data structure that satisfies the heap property. The heap property
+ * for max heap is the following: "if P is a parent node of C, then the
+ * key (the value) of node P is greater than the key of node C"
+ * Source: https://en.wikipedia.org/wiki/Heap_(data_structure)
+ * In-place algorithms 
+ */
+const heapify = function (array, index, heapSize) {
 
   let largest = index;
   let leftIndex = 2 * index + 1;
   let rightIndex = 2 * index + 2;
 
-  if (leftIndex < heapSize && this[leftIndex] > this[largest]) {
+  if (leftIndex < heapSize && array[leftIndex] > array[largest]) {
     largest = leftIndex;
   }
 
-  if (rightIndex < heapSize && this[rightIndex] > this[largest]) {
+  if (rightIndex < heapSize && array[rightIndex] > array[largest]) {
     largest = rightIndex;
   }
 
   if (largest !== index) {
-    let temp = this[largest];
-    this[largest] = this[index];
-    this[index] = temp;
+    array[largest] = array[largest] ^ array[index];
+    array[index] = array[largest] ^ array[index];
+    array[largest] = array[largest] ^ array[index];
 
-    this.heapify(largest, heapSize);
+    heapify(array, largest, heapSize);
   }
 };
 
@@ -38,22 +39,15 @@ function heapSort(items) {
   let length = items.length;
 
   for (let i = Math.floor(items.length / 2) - 1; i > -1; i--) {
-    items.heapify(i, length);
+    heapify(items, i, length);
   }
   for (let j = length -1; j > 0; j--) {
     let tmp = items[0];
     items[0] = items[j];
     items[j] = tmp;
-    items.heapify(0, j);
+    heapify(items, 0, j);
   }
   return items;
 }
 
-//Implementation of heapSort
-
-let ar = [5, 6, 7, 8, 1, 2, 12, 14];
-//Array before Sort
-console.log(ar);
-heapSort(ar);
-//Array after sort
-console.log(ar);
+module.exports = heapSort;
--- algorithms/JavaScript/Kadanes/Kedanes.js
+++ algorithms/JavaScript/Kadanes/Kedanes.js
@@ -1,9 +1,9 @@
 /**
   * Calculates the largest sum of contiguous subarray within a one-dimensional array
   * @param {Array} array - One-dimensional array
-  * @return {Number} currentMax - The largest sum of contiguous subarrays
+  * @return {Number} currentMax - The largest sum of contiguous sub-arrays
   */
-function kadane(array){
+function kadanes(array){
   let currentMax = max = 0;
   for (let i = 0; i < array.length; i++) {
     max = Math.max(0, max + array[i]);
@@ -12,5 +12,4 @@ function kadane(array){
   return currentMax;
 }
 
-let array = [-2, -3, 4, -1, -2, 1, 5, -3];
-console.log("Maximum contiguous sum is: " + kadane(array));
+module.exports = kadanes;
