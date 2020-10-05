12c0969256d1d32b73be081170ba318cb636c521
saketk2019 <56886101+saketk2019@users.noreply.github.com>
2020-10-05 11:48:10 +0530

    Added Factorial in C++ using recursion.

---
+++ C++/Factorial/Factorial.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+     
+long factorial(int);
+ 
+int main()
+{
+  int n;
+  long f;
+ 
+  printf("Enter an integer to find its factorial\n");
+  scanf("%d", &n);
+ 
+  if (n < 0)
+    printf("Factorial of negative integers isn't defined.\n");
+  else
+  {
+    f = factorial(n);
+    printf("%d! = %ld\n", n, f);
+  }
+ 
+  return 0;
+}
+ 
+long factorial(int n)
+{
+  if (n == 0)
+    return 1;
+  else
+    return(n * factorial(n-1));
+}
