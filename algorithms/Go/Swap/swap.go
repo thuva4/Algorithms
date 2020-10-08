5a753adffd84616b7fff0832f63f5373d288c737
ridwanfathin <m.ridwanfathin@gmail.com>
2020-10-08 10:02:36 +0700

    Add swap test

--- algorithms/Go/Swap/swap.go
+++ algorithms/Go/Swap/swap.go
@@ -1,15 +1,15 @@
-package main
+package swap
 
 import "fmt"
 
 func swap(x, y *int) {
-  *x, *y = *y, *x	
+	*x, *y = *y, *x
 }
 
 func main() {
-  x := 3
-  y := 2
-  fmt.Println(x, y)
-  swap(&x, &y)
-  fmt.Println(x, y)
+	x := 3
+	y := 2
+	fmt.Println(x, y)
+	swap(&x, &y)
+	fmt.Println(x, y)
 }
