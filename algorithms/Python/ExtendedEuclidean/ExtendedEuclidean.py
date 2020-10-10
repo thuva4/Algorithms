58e4aa74eecd891ce2309da3ef04a05b4225bfe5
Shehab Hosny <shehab2015@live.com>
2020-10-10 20:03:55 +0200

    Update ExtendedEuclidean.py

--- algorithms/Python/ExtendedEuclidean/ExtendedEuclidean.py
+++ algorithms/Python/ExtendedEuclidean/ExtendedEuclidean.py
@@ -3,7 +3,7 @@ def gcdExtended(x, y):
     if x==0:
         return y,0,1
     # Call the function recursively
-    gcd,x1,y1 = gcdExtended(y%x, x) 
+    gcd,x1,y1 = gcdExtended(y%x, x)
     # Update x2 and y2 using the return of recursive function
     x2 = y1 - (y//x) * x1
     y2 = x1
