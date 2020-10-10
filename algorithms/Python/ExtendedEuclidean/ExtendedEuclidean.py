b84b7c8629c45b6c4cda296179a7bb5e5950ccbe
Shehab Hosny <shehab2015@live.com>
2020-10-10 19:51:10 +0200

    Update ExtendedEuclidean.py

--- algorithms/Python/ExtendedEuclidean/ExtendedEuclidean.py
+++ algorithms/Python/ExtendedEuclidean/ExtendedEuclidean.py
@@ -1,6 +1,6 @@
 def gcdExtended(x, y):
     # Base Condition (Special Case) 
-    if x == 0 :   
+    if x==0 :   
         return y,0,1
     # Call the function recursively
     gcd,x1,y1 = gcdExtended(y%x, x) 
@@ -8,9 +8,9 @@ def gcdExtended(x, y):
     x2 = y1 - (y//x) * x1
     y2 = x1
     return gcd,x2,y2
-      
+
 # Can be modified to be taken as an input from the user
 a = 60
 b = 15
-g,x,y = gcdExtended(a, b)
+g,x,y = gcdExtended(a,b)
 print("gcd(", a , "," , b, ") = ", g)
