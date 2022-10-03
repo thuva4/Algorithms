041ea454e91d6a8bcd542952ea7dc937e3f16cc5
thuva4 <thuva4.dev@gmail.com>
2022-10-03 07:45:56 +0100

    add quick sort tests

--- algorithms/JavaScript/BinarySearch/index.js
+++ algorithms/JavaScript/BinarySearch/index.js
@@ -1,24 +1,17 @@
-/**
- * Search a value into a sorted array by repeatedly
- * dividing the search interval in half.
- * @param  {Array}  arr Array to search into
- * @param  {Number} k Value to search
- * @return {Number} index of found item or -1 for not found
- */
 const binarySearch = (arr, k) => {
-  let min = 0;
-  let max = arr.length - 1;
-
-  while (min <= max) {
-    const cur = Math.floor((min + max) / 2);
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const cur = Math.floor((start + end) /2);
     if (arr[cur] === k) {
-      return cur;
+      return true;
+    } else if (arr[cur] < k) {
+      start = cur + 1;
+    } else {
+      end = cur-1;
     }
-    (arr[cur] > k) ?
-      max = cur - 1 :
-      min = cur + 1;
   }
-  return -1;
+  return false;
 };
 
 module.exports = binarySearch;
--- algorithms/JavaScript/QuickSort/index.js
+++ algorithms/JavaScript/QuickSort/index.js
@@ -1,23 +1,50 @@
-/* eslint-disable require-jsdoc */
-function quickSort(items, left, right) {
-  let index;
+/**
+   * Sort array using quick sort
+   * @param {Array} arr - The array to partition.
+   * @param {Number} left - The left index of the array.
+   * @param {Number} right - The right index of the array.
+   * @return {Number} Sorted array.
+  **/
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    const pivot = partition(arr, left, right);
+    quickSort(arr, left, pivot - 1);
+    quickSort(arr, pivot + 1, right);
+  }
+  return arr;
+};
 
-  if (items.length > 1) {
-    left = typeof left != 'number' ? 0 : left;
-    right = typeof right != 'number' ? items.length - 1 : right;
-
-    index = partition(items, left, right);
-
-    if (left < index - 1) {
-      quickSort(items, left, index - 1);
-    }
-
-    if (index < right) {
-      quickSort(items, index, right);
+/**
+ * Partition array using pivot
+ * @param {Array} arr - The array to partition.
+ * @param {Number} left - The left index of the array.
+ * @param {Number} right - The right index of the array.
+ * @return {Number} The index of the pivot.
+ */
+const partition = (arr, left, right) => {
+  const pivot = arr[right];
+  let i = left;
+  for (let j = left; j < right; j++) {
+    if (arr[j] <= pivot) {
+      swap(arr, i, j);
+      i++;
     }
   }
+  swap(arr, i, right);
+  return i;
+};
 
-  return items;
-}
+/**
+ * Swap two elements in an array
+ * @param {Array} arr - The array to swap.
+ * @param {Number} i - The first index.
+ * @param {Number} j - The second index.
+ */
+const swap = (arr, i, j) => {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
 
-module.exports = {quickSort};
+module.exports = quickSort;
+
